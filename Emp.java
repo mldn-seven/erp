ed856cb0c9010406d22ccf66efe667977f14ba4e
mldn <mldnqa@163.com>
2017-10-11 16:15:13 +0800

    Change Emp.java File

--- Emp.java
+++ Emp.java
@@ -1,5 +1,3 @@
-﻿public class Hello{
-	public static void main(String[] args){
-		System.out.println("Hello GIT!!!");
-	}
+﻿public class Emp implements java.io.Serilizable{
+	private Integer empno;
 }
