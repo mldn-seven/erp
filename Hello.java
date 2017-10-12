0b76e1140fda179cc2f76811325b1032ed991449
mldn <mldnqa@163.com>
2017-10-12 14:18:57 +0800

    Change Hello.java IN master Branch

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 ﻿public class Hello{
 	public static void main(String[] args){
-		//System.out.println("Hello GIT!!!");
 		System.out.println("认真听课的孩子才是好孩子!");
 		System.out.println("不认真听课的孩子去后面站着!");
 		System.out.println("世界，你好!");
-		System.out.println(new java.util.Date());
+		Admin admin=new Admin();
+		Emp emp=new Emp();
 	}
 }
