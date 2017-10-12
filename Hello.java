e9bbf91834e0613877fa67494f9700f6bc361d8c
mldn <mldnqa@163.com>
2017-10-12 14:11:43 +0800

    Change Hello.java IN dev Branch

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
 ﻿public class Hello{
 	public static void main(String[] args){
-		//System.out.println("Hello GIT!!!");
-		System.out.println("认真听课的孩子才是好孩子!");
-		System.out.println("不认真听课的孩子去后面站着!");
-		System.out.println("世界，你好!");
-		System.out.println(new java.util.Date());
+		System.out.println("临近中午，适合学习！");
+
 	}
 }
